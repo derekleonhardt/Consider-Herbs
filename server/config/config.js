0a1c76ced820be51e280b0130d1025978c12abe8
Derek Leonhardt <derek@Dereks-MacBook-Pro-3.local>
2020-02-25 16:01:15 -0500

    Init Commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://derekleonhardt:Ue7wdezr@cen3031-spring-2020-eom5f.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+    }
+};
