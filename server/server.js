bcbe7a5e0ffb03df667dfe96d3e22891262bae42
Hosung <runway3207@hanmail.net>
2020-03-09 19:44:39 -0400

    server stuff

--- server/server.js
+++ server/server.js
@@ -3,10 +3,11 @@ const https = require('https');
 const fs = require('fs');
 const { auth } = require("express-openid-connect");
 const config = require("./config/config.js");
+const sqlite3 = require('sqlite3').verbose();
+//const dbRouter = require('./routes/dbRouter.js')
 
-const key = fs.readFileSync('./localhost-key.pem');
-const cert = fs.readFileSync('./localhost.pem');
-
+const key = fs.readFileSync('./server/localhost-key.pem');
+const cert = fs.readFileSync('./server/localhost.pem');
 const app = express();
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -14,13 +15,24 @@ app.use(auth(config.auth0));
 
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
-  res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
+  res.send(req.isAuthenticated() ? "<button>logout</button>" : "<button>login</button>");
 });
 
+app.get("/check", (req, res) => {
+  res.send(req.isAuthenticated() ? "true" : "false");
+});
+
+<<<<<<< Updated upstream
 app.get("/", (req,res) =>{
 
 });
 
 https.createServer({key, cert}, app).listen('3000', () => {
     console.log('Listening on https://localhost:3000');
+=======
+//app.use('/api/data/', dbRouter);
+
+https.createServer({key, cert}, app).listen('5000', () => {
+    console.log('Listening on https://localhost:5000');
+>>>>>>> Stashed changes
 });
