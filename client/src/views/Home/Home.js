8249fc5d60b3055481086455a8366b2e6221a4d2
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-03-09 16:28:26 -0400

    Sign up on home page fixed

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
 import './Home.css';
-import SignUp from "./components/Header/SignUp";
+import SignUp from "../../components/Header/SignUp";
 
 function Home() {
     return (
