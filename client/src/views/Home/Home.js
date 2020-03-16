2890f3d6a6cd11c0da5e29acffc400d97249a228
Christina <33374101+christinacarlson@users.noreply.github.com>
2020-03-16 16:00:14 -0400

    added photo

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
         <SignUp />
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src="logos/aboutBackground.jpeg" className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
