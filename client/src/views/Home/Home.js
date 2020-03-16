bde7340a6ce51231cf832be9723fd64e832685ff
Christina <33374101+christinacarlson@users.noreply.github.com>
2020-03-16 16:41:19 -0400

    text element

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -7,18 +7,14 @@ function Home() {
     return (
         <>
         <SignUp />
-        <div className="App">
-            <header className="App-header">
+            <div className="About">
                 <img src="logos/aboutBackground.jpeg" className="About-image" />
                 <img src="logos/Dee.jpeg" className="Dee-image" />
-                
-                <p className="about-text">
-                    About
-                </p>
-                <a className="About-info"></a>
-                
-            </header>
-        </div>
+                <p className="about-text">About</p>
+                <figure className="info">
+                    Welcome to my site. My intentions are to present the information in a fun, inviting easy digestible format. One that will reignite that natural innate desire to return to a more holistic earth based approach to our health and well being. Do you remember your first introduction? Some people started with an oil, other with teas or a fresh herb in a dish. I remember how a few sniffs of a peppermint oil suggested by a friend relieved an headache almost instantly. I was hooked and the desire and passion was born to learn , share and help people experience the joy of listening to our body and healing with nature.
+                </figure>   
+            </div>
         </>
     );
 }
