e2f3b6c9b82d50909befda0b153319aef9ba36bf
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-03-16 20:11:57 -0400

    bug fixes

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -8,7 +8,6 @@ function Home(props) {
     return (
         <>
         <SignUp />
-<<<<<<< HEAD
             <div className="About">
                 <img src="logos/aboutBackground.jpeg" className="About-image" />
                 <img src="logos/Dee.jpeg" className="Dee-image" />
@@ -17,10 +16,6 @@ function Home(props) {
                     Welcome to my site. My intentions are to present the information in a fun, inviting easy digestible format. One that will reignite that natural innate desire to return to a more holistic earth based approach to our health and well being. Do you remember your first introduction? Some people started with an oil, other with teas or a fresh herb in a dish. I remember how a few sniffs of a peppermint oil suggested by a friend relieved an headache almost instantly. I was hooked and the desire and passion was born to learn , share and help people experience the joy of listening to our body and healing with nature.
                 </figure>   
             </div>
-=======
-        <div className="App">
-        </div>
->>>>>>> master
         </>
     );
 }
