7c3bbe6ea6d4a3fec11f3036e7ade31a782e8ef5
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-03-30 20:46:53 -0400

    Browse edits

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const https = require('https');
 const fs = require('fs');
 const { auth } = require("express-openid-connect");
 const config = require("./config/config.js");
+var request = require('request');
 // const sqlite3 = require('sqlite3').verbose();
 //const dbRouter = require('./routes/dbRouter.js')
 
@@ -11,17 +12,6 @@ const config = require("./config/config.js");
 
 const app = express();
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config.auth0));
-
-// req.isAuthenticated is provided from the auth router
-app.get("/", (req, res) => {
-  res.send(req.isAuthenticated() ? "<button>logout</button>" : "<button>login</button>");
-});
-
-app.get("/check", (req, res) => {
-  res.send(req.isAuthenticated() ? "true" : "false");
-});
 
 
 (app).listen(5000, () => {
