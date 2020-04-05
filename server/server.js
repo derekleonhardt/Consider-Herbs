68410122920e0c65ee74db19b63a4d933f2af3ae
derekleonhardt <44186328+derekleonhardt@users.noreply.github.com>
2020-04-05 13:11:52 -0400

    heroku test

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const dbRouter = require('./routes/dbRouter.js');
-const app = express();
-//test
+//const path = require('path');
+const express = require('./config/express.js');
+//const bodyParser = require('body-parser');
+//const dbRouter = require('./routes/dbRouter.js');
+
+const app = express.init();
 const cors = require('cors');
 
 app.use(cors());
 
 
 
-app.use(bodyParser.urlencoded({
+/*app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
-app.use('/api/db/', dbRouter);
+//app.use('/api/db/', dbRouter);
 
-app.get("*", (req, res) => {
+/*app.get("*", (req, res) => {
   let url = path.join(__dirname, '../client/build', 'index.html');
   res.sendFile(url);
-});
+});*/
 
 app.listen(process.env.PORT || 5000, () => console.log(`App now listening on port 5000`));
