e8e0f2e566a40d73deb9549beda9abdc89477434
Christina <33374101+christinacarlson@users.noreply.github.com>
2020-04-14 15:35:10 -0400

    button clicking functioning

--- client/src/views/Remedy/Remedy.js
+++ client/src/views/Remedy/Remedy.js
@@ -21,29 +21,29 @@ const Register = (props) => {
                     <img src="logos/body-1.jpg" className="body"/> 
                     
                     
-                    <button className="arm">Arm</button>
-                    <button className="back">Back</button>
-                    <button className="chest">Chest</button>
-                    <button className="ears">Ears</button>
-                    <button className="eyes">Eyes</button>
-                    <button className="face">Face</button>
-                    <button className="feet">Feet</button>
-                    <button className="fingers">Fingers</button>
-                    <button className="hands">Hands</button>
-                    <button className="head">Head</button>
-                    <button className="knee">Knee</button>
-                    <button className="legs">Legs</button>
-                    <button className="lips">Lips</button>
-                    <button className="mouth">Mouth</button>
-                    <button className="stomach">Stomach</button>
-                    <button className="throat">Throat</button>
-                    <button className="wrist">Wrist</button>
+                    <button className="arm" value="arm" onClick = {e => props.searchRecipeByBody(e, setResults)}>Arm</button>
+                    <button className="back" value="back" onClick = {e => props.searchRecipeByBody(e, setResults)}>Back</button>
+                    <button className="chest" value="chest" onClick = {e => props.searchRecipeByBody(e, setResults)}>Chest</button>
+                    <button className="ears" value="ears" onClick = {e => props.searchRecipeByBody(e, setResults)}>Ears</button>
+                    <button className="eyes" value="eyes" onClick = {e => props.searchRecipeByBody(e, setResults)}>Eyes</button>
+                    <button className="face" value="face" onClick = {e => props.searchRecipeByBody(e, setResults)}>Face</button>
+                    <button className="feet" value="feet" onClick = {e => props.searchRecipeByBody(e, setResults)}>Feet</button>
+                    <button className="fingers" value="fingers" onClick = {e => props.searchRecipeByBody(e, setResults)}>Fingers</button>
+                    <button className="hands" value="hands" onClick = {e => props.searchRecipeByBody(e, setResults)}>Hands</button>
+                    <button className="head" value="head" onClick = {e => props.searchRecipeByBody(e, setResults)}>Head</button>
+                    <button className="knee" value="knee" onClick = {e => props.searchRecipeByBody(e, setResults)}>Knee</button>
+                    <button className="legs" value="legs" onClick = {e => props.searchRecipeByBody(e, setResults)}>Legs</button>
+                    <button className="lips" value="lips" onClick = {e => props.searchRecipeByBody(e, setResults)}>Lips</button>
+                    <button className="mouth" value="mouth" onClick = {e => props.searchRecipeByBody(e, setResults)}>Mouth</button>
+                    <button className="stomach" value="stomach" onClick = {e => props.searchRecipeByBody(e, setResults)}>Stomach</button>
+                    <button className="throat" value="throat" onClick = {e => props.searchRecipeByBody(e, setResults)}>Throat</button>
+                    <button className="wrist" value="wrist" onClick = {e => props.searchRecipeByBody(e, setResults)}>Wrist</button>
                     
                     
                     <div className="inspoTitle"> Suggested Recipes </div>
                     <div className ="searchBar">
                     <img src = "logos/search.png"/>
-                    <input type = "text" placeholder = "Search For Herbs" onChange = {e => props.searchRecipeByBody(e, setResults)}/>
+                    <input type = "text" placeholder = "Search By Ailment" onChange = {e => props.searchRecipeByBody(e, setResults)}/>
                     </div> 
                     <BrowseRecipe
                         results = {results.data}
