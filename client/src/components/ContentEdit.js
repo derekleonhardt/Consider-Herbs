499460d308e4d58cb33c2b39b1289d0f6fe7bb39
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-04-20 16:26:55 -0400

    admin styling complete

--- client/src/components/ContentEdit.js
+++ client/src/components/ContentEdit.js
@@ -3,34 +3,10 @@ import HomeSub from './HomeSub';
 import './ContentEdit.css';
 
 const ContentEdit = (props) => {
-    const pages = ["Home (Subscriber)","Home (Guest)", "Browse", "Remedy", "Book"];
-    const [pageToEdit, setPage] = useState(pages[0]);
-    let pageOptions = pages.map((page, index) => {
-        return(
-            <option key = {index} value = {page}>
-                {page}
-            </option>
-        );
-    });
-    let ShownPage = HomeSub;
-    {
-        switch(pageToEdit){
-            case pages[0]:
-                ShownPage = HomeSub;
-                break;
-        }
-    }
     return(
         <div className = "contentEdit adminPanel">
             <h2 className = "adminHeader">Manage Content</h2>
-
-            <label>Page to be edited</label>
-            <select onChange = {e => {
-                // setPage(e.target.value);
-            }}>
-                {pageOptions}
-            </select>
-            <ShownPage 
+            <HomeSub 
             getDbListings = {props.getDbListings}
             updateDbListings = {props.updateDbListings}
             deleteDbListings = {props.deleteDbListings}
